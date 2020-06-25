c30893760a28a212afa7fce9549f52d7a32e00f2
Iury Vieira Lippo da Silva <iuryvieira.lippo@gmail.com>
2020-06-25 19:41:17 -0300

    Updates

---
+++ precache-manifest.1ed01de3b1df7a43643b0c2e76fff8fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2e69af5b0da8d22782a4b9a7068bd26",
+    "url": "/testCesar/index.html"
+  },
+  {
+    "revision": "6c854094c7e21c9619ff",
+    "url": "/testCesar/static/css/main.e2904489.chunk.css"
+  },
+  {
+    "revision": "fa7bcd32dcd5c1d68a46",
+    "url": "/testCesar/static/js/2.2d50dabc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/testCesar/static/js/2.2d50dabc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c854094c7e21c9619ff",
+    "url": "/testCesar/static/js/main.28bb6974.chunk.js"
+  },
+  {
+    "revision": "4f5927ae9d804c3af55b",
+    "url": "/testCesar/static/js/runtime-main.f8ac098f.js"
+  }
+]);
